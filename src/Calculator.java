614c3ecd3d731b31e8c082aaacb0b3d08c3a70e2
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2025-09-22 02:32:46 +0530

    Color changes

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,7 +9,7 @@ class Calculator extends JFrame {
         this.setTitle("Basic Calulator");
         this.setSize(400, 600);
         this.setResizable(false);
-        this.getContentPane().setBackground(new Color(55, 59, 56));
+        this.getContentPane().setBackground(new Color(120, 128, 122));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         
@@ -18,6 +18,14 @@ class Calculator extends JFrame {
         for (int i = 0; i < 20; i++) {
             button[i] = new JButton(symbols[i]);
             button[i].setBounds(buttonX, buttonY, 96, 96);
+            button[i].setBackground(new Color(188, 196, 190));
+            button[i].setForeground(Color.BLACK);
+            Font tempFont = button[i].getFont();
+            Font buttonFont = tempFont.deriveFont(20f);
+            button[i].setFont(buttonFont);
+
+            this.add(button[i]);
+
             if (buttonX < 200) {
                 buttonX += 96;
             }
@@ -25,7 +33,6 @@ class Calculator extends JFrame {
                 buttonX = 0;
                 buttonY += 96;
             }
-            this.add(button[i]);
         }
         
         this.setVisible(true);
