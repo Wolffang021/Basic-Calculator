0e5b7e2f99a9ecc95a987c3e213d3696285637f8
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2025-09-22 02:46:45 +0530

    Added Displays

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,6 +12,17 @@ class Calculator extends JFrame {
         this.getContentPane().setBackground(new Color(120, 128, 122));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
+
+        JLabel prevDisplay = new JLabel("12732.3434");
+        prevDisplay.setBounds(0, 0, 400, 30);
+        this.add(prevDisplay);
+        
+        JLabel curDisplay = new JLabel("12732.3434");
+        curDisplay.setBounds(0, 30, 400, 50);
+        Font tempCurDisplayFont = curDisplay.getFont();
+        Font curDisplayFont = tempCurDisplayFont.deriveFont(50f);
+        curDisplay.setFont(curDisplayFont);
+        this.add(curDisplay);
         
         String[] symbols = {"C", "()", "%", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ".", "="};
         JButton[] button = new JButton[20];
@@ -21,7 +32,7 @@ class Calculator extends JFrame {
             button[i].setBackground(new Color(188, 196, 190));
             button[i].setForeground(Color.BLACK);
             Font tempFont = button[i].getFont();
-            Font buttonFont = tempFont.deriveFont(20f);
+            Font buttonFont = tempFont.deriveFont(25f);
             button[i].setFont(buttonFont);
 
             this.add(button[i]);
